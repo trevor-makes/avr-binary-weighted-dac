9efe973cd30b6a6daeb3bdede10d064360e645d1
Trevor Makes <the.trevor.makes@gmail.com>
2022-02-08 16:44:08 -0800

    Move bitmap ROMs to rom_data.cpp file

---
+++ src/rom_data.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <avr/pgmspace.h>
+
+// Commodore 64 font extracted from VICE
+extern const uint8_t CHAR_ROM[] PROGMEM;
+
+// 64x64 1-bit Doge bitmap
+extern const uint8_t DOGE_ROM[] PROGMEM;
+
+// 64x64 1-bit Pepe bitmap
+extern const uint8_t PEPE_ROM[] PROGMEM;
